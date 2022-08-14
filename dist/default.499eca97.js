5f378c572c5785fef33a37d8d6bf920fffaf98af
akaryaka <a.karyaka.2dit@gmail.com>
2022-08-14 23:37:55 +0600

    form-elements: dropdown

--- dist/default.499eca97.js
+++ dist/default.499eca97.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./images\\arrow_forward-white.svg":[["arrow_forward-white.de3f991e.svg","../../images/arrow_forward-white.svg"],"../../images/arrow_forward-white.svg"],"_css_loader":"C:/Users/User/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"C:/Users/User/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./images\\arrow_forward-white.svg":[["arrow_forward-white.de3f991e.svg","../images/arrow_forward-white.svg"],"../images/arrow_forward-white.svg"],"_css_loader":"C:/Users/User/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"C:/Users/User/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49344" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58836" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
