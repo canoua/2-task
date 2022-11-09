43e42d9e2bb30b263a28ab62251f195f48f07b5e
akaryaka <a.karyaka.2dit@gmail.com>
2022-11-10 00:56:44 +0600

    air-datapicker styles

--- src/scripts/modules/air-datepicker.js
+++ src/scripts/modules/air-datepicker.js
@@ -1,9 +1,22 @@
 import AirDatepicker from 'air-datepicker';
 
 export default function airDatePicker() {
+  let button = {
+    className: 'custom-button-classname',
+  }
+
+  // let arrows = document.querySelector('.air-datepicker-nav--action');
+  
   new AirDatepicker('.calendar', {
     selectedDates: [new Date()],
     range: true,
-    multipleDatesSeparator: ' - '
+    multipleDatesSeparator: ' - ',
+    navTitles: {
+        days: '<div>MMMM yyyy</div>'
+    },
+    buttons: ['clear'],
+    visible: true
   });
+
+  // console.log(arrows.childNodes);
 }
