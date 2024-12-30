9a8cb6677e7e9e1b1d6301f4d2e3c32ddb2f8f5a
akaryaka <a.karyaka.2dit@gmail.com>
2024-12-30 23:57:11 +0600

    card landing ref: dropdown

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,7 @@ import rangeSlider from './modules/range-slider';
 import mask from './modules/mask';
 import searchDropdown from './modules/search-dropdown';
 import dropdownCounter from './modules/dropdown-counter';
+import landing from './modules/landing';
 // import airDatePicker from './modules/air-datepicker';
 
 'use strict';
@@ -10,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if(document.querySelector('#range-slider')) rangeSlider();  
   if(document.querySelector('.card__input-masked')) mask();
   if(document.querySelector('.expandable-checkbox-list-default')) searchDropdown();
-  if(document.querySelector('.dropdown-expanded-guests__form ')) dropdownCounter();
+  if(document.querySelector('.dropdown-expanded-guests__form')) dropdownCounter();
+  if(document.querySelector('.main-landing')) landing();
   // if(document.querySelector('.calendar')) airDatePicker();
 })
---
+++ src/scripts/modules/landing.js
@@ -0,0 +1,3 @@
+export default function landing() {
+  console.log('landing');
+}
