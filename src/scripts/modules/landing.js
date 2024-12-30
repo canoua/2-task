3079cd5525274cd84b3dd18486424c49dc6e0f9c
akaryaka <a.karyaka.2dit@gmail.com>
2024-12-31 00:19:08 +0600

    disclosure dropdown

--- src/scripts/modules/landing.js
+++ src/scripts/modules/landing.js
@@ -1,3 +1,8 @@
-export default function landing() {
-  console.log('landing');
+export default function landing() {  
+  let dropdown = document.querySelector('.card-landing__dropdown');
+  let dropdownContent = document.querySelector('.fe__dropdown__content');
+  // fe__dropdown__content
+  dropdown.addEventListener('click', function() {
+    dropdownContent.classList.toggle('fe__dropdown__content_active')
+  })
 }
