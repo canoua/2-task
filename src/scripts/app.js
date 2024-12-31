88d7f63537d490bd12898ae1af494092db5a7b29
akaryaka <a.karyaka.2dit@gmail.com>
2024-12-31 23:34:22 +0600

    add counter in dropdown(landing)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,6 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
   if(document.querySelector('.card__input-masked')) mask();
   if(document.querySelector('.expandable-checkbox-list-default')) searchDropdown();
   if(document.querySelector('.dropdown-expanded-guests__form')) dropdownCounter();
+  // упростить
+  if(document.querySelector('.dropdown-list__count')) dropdownCounter();
   if(document.querySelector('.main-landing')) landing();
   // if(document.querySelector('.calendar')) airDatePicker();
 })
