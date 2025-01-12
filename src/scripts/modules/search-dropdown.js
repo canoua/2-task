efe0fdc5014a01f02918edc10b06b5e49229ee5a
akaryaka <a.karyaka.2dit@gmail.com>
2025-01-12 23:50:54 +0600

    begin js-logic dropdown features in search-page

--- src/scripts/modules/search-dropdown.js
+++ src/scripts/modules/search-dropdown.js
@@ -5,14 +5,22 @@ export default function searchDropdown() {
   let dropdownConvenience = document.querySelector('.dropdown-expanded-guests__form > .dropdown-convenience');
     // выпадающий блок
   let dropdownContent = document.querySelector('.dropdown-convenience__content');
+  let convenienceCount = document.querySelectorAll('.dropdown__convenience-list__count_number');
 
+  let btnPlus = document.querySelectorAll('.dropdown__convenience-list__count_plus');
+  
+  let convenience = {
+    bedrooms: 0,
+    beds: 0,
+    bathrooms: 0
+  }
+  
+    
   input.addEventListener('click', function() {
-    // console.log('dropdown');
     dropdown.classList.toggle('search__checkbox__list_active')
   })
 
   dropdownConvenience.addEventListener('click', function() {
-    // console.log('удобства');
     dropdownContent.classList.toggle('fe__dropdown__content_active');
   })
 }
