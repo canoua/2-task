d6e4e79b23cb579c3b8ec8db56820bcc5fc47ea4
akaryaka <a.karyaka.2dit@gmail.com>
2025-01-22 11:47:46 +0600

    ref search-dropdown 22.01

--- src/scripts/modules/search-dropdown.js
+++ src/scripts/modules/search-dropdown.js
@@ -5,35 +5,35 @@ export default function searchDropdown() {
   let dropdownConvenience = document.querySelector('.dropdown-expanded-guests__form > .dropdown-convenience');
   let dropdownConvenienceOutput = document.querySelector('.dropdown-expanded-guests__form > .dropdown-convenience > .card__input-wrapper > .card__input-dropdown');
   
-    // выпадающий блок
+  // выпадающий блок
   let dropdownContent = document.querySelector('.dropdown-convenience__content');
   // let convenienceCount = document.querySelectorAll('.dropdown__convenience-list__count_number');
 
   let btnPlus = document.querySelectorAll('.dropdown__convenience-list__count_plus');
   let btnMinus = document.querySelectorAll('.dropdown__convenience-list__count_minus');
   let resultCounter = document.querySelectorAll('.dropdown__convenience-list__count_number');
+
+  let bedroomsCount = document.querySelector('.dropdown__convenience-list__count_bedrooms');
+  let bathroomsCount = document.querySelector('.dropdown__convenience-list__count_bathrooms');
+  let bedsCount = document.querySelector('.dropdown__convenience-list__count_beds');
   
   let convenience = {
-    bedrooms: 2,
-    beds: 2,
-    bathrooms: 0
+    bedrooms: 1,
+    bathrooms: 2,
+    beds: 0
   }
 
   function view() {
     window.addEventListener('load', function() {
-      dropdownConvenienceOutput.textContent = `${convenience.bedrooms}спальни`;
+      bedroomsCount.textContent = convenience.bedrooms;
+      bathroomsCount.textContent = convenience.bathrooms;
+      bedsCount.textContent = convenience.beds;
+      dropdownConvenienceOutput.textContent = `${convenience.bedrooms} спальни`;
     })
   }
 
   view();
- 
-  function sum() {
-    let sum = 0;
-    resultCounter.forEach(function(item) {
-      // sum += parseInt(item.textContent);
-    })
-    return sum;
-  } 
+
 
   btnPlus.forEach(function(item) {
     item.addEventListener('click', function(event) {
@@ -67,18 +67,21 @@ export default function searchDropdown() {
       // console.log(item.parentElement.parentElement.firstElementChild.textContent);
       if(item.parentElement.parentElement.firstElementChild.textContent == 'спальни') {
         convenience.bedrooms--;
+        dropdownConvenienceOutput.textContent = `${convenience.bedrooms} спальни`;
         console.log(convenience.bedrooms);
       } else if(item.parentElement.parentElement.firstElementChild.textContent == 'кровати') {
         convenience.bathrooms--;
+        dropdownConvenienceOutput.textContent = `${convenience.bathrooms} кровати`;
         console.log(convenience.bathrooms);
       } else if(item.parentElement.parentElement.firstElementChild.textContent == 'ванные комнаты') {
         convenience.beds--;
+        dropdownConvenienceOutput.textContent = `${convenience.beds} ванные комнаты`;
         console.log(convenience.bathrooms);
       }
     })
   })
   
-    
+  
   input.addEventListener('click', function() {
     dropdown.classList.toggle('search__checkbox__list_active')
   })
