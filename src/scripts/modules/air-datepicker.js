c7f988b3a27d9c025e8a18066796c25a2d800df7
akaryaka <a.karyaka.2dit@gmail.com>
2025-02-17 22:01:21 +0600

    add inputs features on calendar

--- src/scripts/modules/air-datepicker.js
+++ src/scripts/modules/air-datepicker.js
@@ -3,51 +3,56 @@ import AirDatepicker from 'air-datepicker';
 export default function airDatePicker() {
   let buttonApply = {
     className: 'buttonAirData',
-    content: 'Применить'
+    content: 'Применить',
+    onClick: () => {
+      let input1 = document.querySelector('.input1');
+      let input2 = document.querySelector('.input2');
+      // input2.value = calendar.selectedDates[1];
+      let dateString = calendar.selectedDates[0]
+      // const dayOfMonth = date.getDate();
+      console.log(dateString.getDate());
+      // попробовать использовать это
+      /*
+      var options = {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric'
+      }
+
+      function getDate(str) {
+        var date = new Date(str);
+        return date.toLocaleString('ru', options)
+      }
+
+      console.log(getDate('2017-09-21T21:00:00.000Z'));
+            
+      */
+    },
   }
 
   let button = {
     content: 'Select 2021-07-26',
     className: 'custom-button-classname',
-    onClick: (dp) => {
-        let date = new Date('2021-07-26');
-        dp.selectDate(date);
-        // let dateN = dp.selectDate(date);
-        console.log(startDate);
-        console.log(event.target);
-        
-        // dp.setViewDate(date);
-    }
   }
 
-  let startDate;
+  let startDate, endDate;
   
   let calendar = new AirDatepicker('.calendar', {
     selectedDates: [new Date()],
     range: true,
-    dynamicRange: true,
-    multipleDates: true,
-    multipleDatesSeparator: '',
+    dynamicRange: false,
+    multipleDatesSeparator: '-',
     startDate,
+    endDate,
+    dateFormat: 'dd.MM.yyyy',
     navTitles: {
       days: '<div>MMMM yyyy</div>'
     },
     buttons: [button, buttonApply],
-    selectedDates: [startDate, '2021-07-25']
+    selectedDates: [startDate, endDate],
+    onSelect: (date)=> {
+      // попытка не выводить дату в инпут   
+      console.log("Выбранная дата:", date); // Для отладки, если нужно
+    },
   });
-
-  startDate = new Date(calendar.selectedDates[0]);
 }
-
-  // new AirDatepicker('.calendar', {
-  //   selectedDates: [new Date()],
-  //   range: true,
-  //   dynamicRange: true,
-  //   multipleDates: true,
-  //   multipleDatesSeparator: '',
-  //   navTitles: {
-  //     days: '<div>MMMM yyyy</div>'
-  //   },
-  //   buttons: [button, buttonApply],
-  //   visible: true,
-  // });
