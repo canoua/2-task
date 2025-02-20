6b900a1e673a9bcd170b1bbf5a98da2e15b4c29b
akaryaka <a.karyaka.2dit@gmail.com>
2025-02-20 20:20:20 +0600

    bug button fixed 20.02, 20:20

--- src/scripts/modules/air-datepicker.js
+++ src/scripts/modules/air-datepicker.js
@@ -4,18 +4,42 @@ export default function airDatePicker() {
   let buttonApply = {
     className: 'buttonAirData',
     content: 'Применить',
-    onClick: () => {
+    onClick: function() {
+    
       let input1 = document.querySelector('.input1');
       let input2 = document.querySelector('.input2');
-      input1.value = calendar.selectedDates[0];
-      input2.value = calendar.selectedDates[1];
+
+      let day1 = calendar.selectedDates[0].toLocaleString("default", { day: "2-digit" });
+      let month1 = calendar.selectedDates[0].toLocaleString("default", { month: "2-digit" });
+      let year1 = calendar.selectedDates[0].toLocaleString("default", { year: "numeric" });
+
+      let day2 = calendar.selectedDates[1].toLocaleString("default", { day: "2-digit" });
+      let month2 = calendar.selectedDates[1].toLocaleString("default", { month: "2-digit" });
+      let year2 = calendar.selectedDates[1].toLocaleString("default", { year: "numeric" });
+      
+      input1.value = `${day1}.${month1}.${year1}`;
+      input2.value = `${day2}.${month2}.${year2}`;
+      // input1.value = val1;
+      // if(val2!=undefined) {
+        // input2.value = val2;
+      // } else {
+        // input2.value = '';
+      // }
+      console.log(event.target);
+      
     },
+    // () => {
+    //   let input1 = document.querySelector('.input1');
+    //   let input2 = document.querySelector('.input2');
+    //   input1.value = calendar.selectedDates[0];
+    //   input2.value = calendar.selectedDates[1];
+    // },
   }
 
-  let button = {
-    content: 'Select 2021-07-26',
-    className: 'custom-button-classname',
-  }
+  // let button = {
+  //   content: 'Select 2021-07-26',
+  //   className: 'custom-button-classname',
+  // }
 
   let startDate, endDate;
   
@@ -30,10 +54,9 @@ export default function airDatePicker() {
     navTitles: {
       days: '<div>MMMM yyyy</div>'
     },
-    buttons: [button, buttonApply],
+    buttons: ['clear', buttonApply],
     selectedDates: [startDate, endDate],
     onSelect: function(date, formattedDate, datepicker) {
-    
       let input1 = document.querySelector('.input1');
       let input2 = document.querySelector('.input2');
       
