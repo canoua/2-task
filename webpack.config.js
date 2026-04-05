809a14666152dea5c61a4880cf808f6739c74929
Andrew <akariaka@yandex.ru>
2026-04-05 11:43:52 +0600

    webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
-const path = require('path');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+const path = require("path");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'main.[contenthash].js',
+    path: path.join(__dirname, "dist"),
+    filename: "main.[contenthash].js",
     // сделано, для того, чтобы структура папок из src повторилась в dist
-    assetModuleFilename: pathData => {
-      const filepath = path.dirname(pathData.filename).split('/').slice(1).join('/');
+    assetModuleFilename: (pathData) => {
+      const filepath = path
+        .dirname(pathData.filename)
+        .split("/")
+        .slice(1)
+        .join("/");
       return `${filepath}/[name][ext]`;
-    }
+    },
   },
-  devtool: 'source-map',
+  devtool: "cheap-module-source-map",
+  // devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.(png|jpg|jpeg|svg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.pug$/,
-        loader: 'pug-loader',
+        loader: "pug-loader",
       },
       {
         test: /\.(scss|css)$/,
@@ -41,83 +46,99 @@ module.exports = {
               sourceMap: true,
             },
           },
-          'postcss-loader',
+          "postcss-loader",
           {
             loader: "sass-loader",
             options: {
               sourceMap: true,
               sassOptions: {
                 outputStyle: "compressed",
-              }
+              },
             },
           },
         ],
       },
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.pug'),
-      filename: 'index.html',
+      template: path.join(__dirname, "src", "index.pug"),
+      filename: "index.html",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/ui-pages/headers-footers', 'headers-footers.pug'),
-      filename: 'ui-pages/headers-footers.html',
+      template: path.join(
+        __dirname,
+        "src/ui-pages/headers-footers",
+        "headers-footers.pug",
+      ),
+      filename: "ui-pages/headers-footers.html",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/ui-pages/form-elements', 'form-elements(fe).pug'),
-      filename: 'ui-pages/form-elements(fe).html',
+      template: path.join(
+        __dirname,
+        "src/ui-pages/form-elements",
+        "form-elements(fe).pug",
+      ),
+      filename: "ui-pages/form-elements(fe).html",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/ui-pages/colors-types', 'colors-types.pug'),
-      filename: 'ui-pages/colors-types.html',
+      template: path.join(
+        __dirname,
+        "src/ui-pages/colors-types",
+        "colors-types.pug",
+      ),
+      filename: "ui-pages/colors-types.html",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/ui-pages/cards', 'cards.pug'),
-      filename: 'ui-pages/cards.html',
+      template: path.join(__dirname, "src/ui-pages/cards", "cards.pug"),
+      filename: "ui-pages/cards.html",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/pages/sign-in', 'sign-in.pug'),
-      filename: 'pages/sign-in.html',
+      template: path.join(__dirname, "src/pages/sign-in", "sign-in.pug"),
+      filename: "pages/sign-in.html",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/pages/landing', 'landing.pug'),
-      filename: 'pages/landing.html',
+      template: path.join(__dirname, "src/pages/landing", "landing.pug"),
+      filename: "pages/landing.html",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/pages/search', 'search.pug'),
-      filename: 'pages/search.html',
+      template: path.join(__dirname, "src/pages/search", "search.pug"),
+      filename: "pages/search.html",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/pages/sign-up', 'sign-up.pug'),
-      filename: 'pages/sign-up.html',
+      template: path.join(__dirname, "src/pages/sign-up", "sign-up.pug"),
+      filename: "pages/sign-up.html",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/pages/room-details', 'room-details.pug'),
-      filename: 'pages/room-details.html',
+      template: path.join(
+        __dirname,
+        "src/pages/room-details",
+        "room-details.pug",
+      ),
+      filename: "pages/room-details.html",
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
+      filename: "[name].[contenthash].css",
     }),
     new FileManagerPlugin({
       events: {
         onStart: {
-          delete: ['dist'],
+          delete: ["dist"],
         },
       },
     }),
   ],
   devServer: {
-    watchFiles: path.join(__dirname, 'src'),
+    watchFiles: path.join(__dirname, "src"),
     port: 9000,
   },
   resolve: {
     alias: {
-      "@images": path.resolve(__dirname, "src/assets/images/")
+      "@images": path.resolve(__dirname, "src/assets/images/"),
     },
   },
   optimization: {
@@ -127,14 +148,15 @@ module.exports = {
           implementation: ImageMinimizerPlugin.imageminMinify,
           options: {
             plugins: [
-              ['gifsicle', { interlaced: true }],
-              ['jpegtran', { progressive: true }],
-              ['optipng', { optimizationLevel: 5 }],
-              ['svgo', { name: 'preset-default' }],
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              ["svgo", { name: "preset-default" }],
             ],
           },
         },
       }),
     ],
   },
+  cache: { type: "filesystem" },
 };
