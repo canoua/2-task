b36de27c4477dde70475c7ddde92a448daf7bc21
akariaka@yandex.ru <akariaka@yandex.ru>
2026-05-29 03:20:29 +0600

    ref project: done burger

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,21 @@
-import rangeSlider from './modules/range-slider';
-import mask from './modules/mask';
-import searchDropdown from './modules/search-dropdown';
-import dropdownCounter from './modules/dropdown-counter';
-import dropdownGuests from './modules/dropdown-guests';
-import airDatePicker from './modules/air-datepicker';
+import rangeSlider from "./modules/range-slider";
+import mask from "./modules/mask";
+import searchDropdown from "./modules/search-dropdown";
+import dropdownCounter from "./modules/dropdown-counter";
+import dropdownGuests from "./modules/dropdown-guests";
+import airDatePicker from "./modules/air-datepicker";
+import burger from "./modules/burger";
 
-'use strict';
+("use strict");
 
-document.addEventListener('DOMContentLoaded', () => {
-  if(document.querySelector('#range-slider')) rangeSlider();  
-  if(document.querySelector('.card__input-masked')) mask();
-  if(document.querySelector('.expandable-checkbox-list-default')) searchDropdown();
-  if(document.querySelector('.dropdown-expanded-guests__form')) dropdownCounter();
-  if(document.querySelector('.app-js-dropdown')) dropdownGuests();
-  if(document.querySelector('.calendar')) airDatePicker();
-})
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.querySelector("#range-slider")) rangeSlider();
+  if (document.querySelector(".card__input-masked")) mask();
+  if (document.querySelector(".expandable-checkbox-list-default"))
+    searchDropdown();
+  if (document.querySelector(".dropdown-expanded-guests__form"))
+    dropdownCounter();
+  if (document.querySelector(".app-js-dropdown")) dropdownGuests();
+  if (document.querySelector(".calendar")) airDatePicker();
+  if (document.querySelector(".burger")) burger();
+});
