e4504a6570dada6d23c23a4c572b44c6197c962c
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-14 22:42:46 +0600

    ref: mask range

--- src/scripts/modules/mask.js
+++ src/scripts/modules/mask.js
@@ -1,8 +1,26 @@
 import IMask from "imask";
 
 export default function mask() {
+  const currentYear = new Date().getFullYear() - 1;
   const inputMask = document.querySelector(".card__input-masked");
   const mask = new IMask(inputMask, {
-    mask: "00.00.0000",
+    mask: "DD.MM.YYYY",
+    blocks: {
+      DD: {
+        mask: IMask.MaskedRange,
+        from: 1,
+        to: 31,
+      },
+      MM: {
+        mask: IMask.MaskedRange,
+        from: 1,
+        to: 12,
+      },
+      YY: {
+        mask: IMask.MaskedRange,
+        from: 1910,
+        to: currentYear,
+      },
+    },
   });
 }
