8fae1c4395221f72536d98833461fc6318c21fa2
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-23 20:50:22 +0600

    search page: bug fixed calendar

--- src/scripts/pages/search.js
+++ src/scripts/pages/search.js
@@ -251,8 +251,71 @@ export default function search() {
   });
 
   let input1 = document.querySelector(".card__input-wrapper");
+  let startDate, secondDate, endDate;
+
+  let buttonApply = {
+    className: "buttonAirData",
+    content: "Применить",
+    onClick: function () {
+      let day1 = calendar.selectedDates[0].toLocaleString("default", {
+        day: "2-digit",
+      });
+      let month1 = calendar.selectedDates[0].toLocaleString("ru-RU", {
+        month: "long",
+      });
+
+      let day2 = calendar.selectedDates[1].toLocaleString("default", {
+        day: "2-digit",
+      });
+      let month2 = calendar.selectedDates[1].toLocaleString("ru-RU", {
+        month: "long",
+      });
+
+      // input1.value = `${day1}.${month1}`;
+      calendar.hide();
+    },
+  };
 
   let calendar = new AirDatepicker(".calendar", {
-    visible: false,
+    selectedDates: [new Date()],
+    range: true,
+    classes: "air-datepicker-custom",
+    dynamicRange: false,
+    multipleDates: true,
+    multipleDatesSeparator: "-",
+    startDate,
+    secondDate: new Date(),
+    endDate,
+    dateFormat: "dd MM",
+    navTitles: {
+      days: '<div class = "air-datepicker-navTitle-custom">MMMM yyyy</div>',
+    },
+    buttons: ["clear", buttonApply],
+    selectedDates: [startDate, secondDate, endDate],
+    onSelect: function (date) {
+      let val1 = date.formattedDate[0];
+      let val2 = date.formattedDate[1];
+
+      if (date.formattedDate[1] != undefined) {
+        input1.value = val1;
+      }
+    },
+    visible: true,
+    position({ $datepicker, $target, $pointer }) {
+      // координаты инпута
+      let coords = $target.getBoundingClientRect();
+      let top = coords.y + 2 * coords.height + window.scrollY;
+      let left = coords.x - 17;
+
+      $datepicker.style.left = `${left}px`;
+      $datepicker.style.top = `${top}px`;
+      $pointer.style.display = "none";
+    },
+    onShow: (isFinished) => {
+      const calendarContent = document.querySelector(".air-datepicker");
+      if (calendarContent) {
+        calendarContent.style.width = "320px";
+      }
+    },
   });
 }
