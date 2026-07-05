608dad00f706bad85fbc35abcad5356b04559ae6
akariaka@yandex.ru <akariaka@yandex.ru>
2026-07-05 23:46:22 +0600

    delete comments

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,4 @@
-import rangeSlider from "./modules/range-slider";
-import search from "./pages/search";
-import dropdownCounter from "./modules/dropdown/dropdownCounter";
-import dropdownGuests from "./modules/dropdown-guests";
-import airDatePicker from "./libs/air-datepicker";
-import burger from "./modules/burger";
+import search from "./pages/search/search";
 import roomDetails from "./pages/roomDetails";
 import landing from "./pages/landing";
 import signUp from "./pages/signUp";
@@ -14,10 +9,6 @@ import signIn from "./pages/signIn";
 
 document.addEventListener("DOMContentLoaded", () => {
   if (document.querySelector("#search-js")) search();
-  // if (document.querySelector(".dropdown-expanded-guests__form"))
-  //   dropdownCounter();
-  // if (document.querySelector(".app-js-dropdown")) dropdownGuests();
-  // if (document.querySelector(".burger")) burger();
   if (document.querySelector("#rd-dropdown")) roomDetails();
   if (document.querySelector("#sign-up-js")) signUp();
   if (document.querySelector("#landing-js")) landing();
