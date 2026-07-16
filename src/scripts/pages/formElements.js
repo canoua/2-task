886070aceecf7ea5a78113aca7d84895cfec61f0
Andrey <akariaka@yandex.ru>
2026-07-16 19:39:16 +0600

    add .pagination__link_active in form-elements.pug

--- src/scripts/pages/formElements.js
+++ src/scripts/pages/formElements.js
@@ -2,4 +2,8 @@ import rangeSlider from "../modules/range-slider";
 
 export default function formElements() {
   if (document.querySelector("#range-slider")) rangeSlider();
+  if (document.querySelector('.fe__pagination')) {
+    const paginationListItemActive = document.querySelector('.pagination__list').firstChild;
+    paginationListItemActive.querySelector('.pagination__link').classList.add('pagination__link_active'); 
+  }
 }
